4c261338bcf184ae1d61921503d24b19db0ad68d
Oyindamola Oyetunmibi <oyindadavid18@gmail.com>
2023-06-27 01:56:12 +0100

    added spacing and styling for the main section

--- src/features/cabins/CabinRow.jsx
+++ src/features/cabins/CabinRow.jsx
@@ -1,4 +1,5 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 import styled from "styled-components";
 import { deleteCabin } from "../../services/apiCabins";
 
@@ -58,10 +59,13 @@ const CabinRow = ({ cabin }) => {
   const { isLoading: isDeleting, mutate } = useMutation({
     mutationFn: deleteCabin,
     onSuccess: () => {
+      toast.success("Cabin successfully deleted");
+
       queryClient.invalidateQueries({
         queryKey: ["cabins"],
       });
     },
+    onError: (err) => toast.error(err.message),
   });
 
   return (
